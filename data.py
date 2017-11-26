f09efcc6a0553470e2a7fed6fcfd278aa07f57ba
JhumanJ <julien@nahum.net>
2017-11-26 19:25:08 +0000

    Version log

--- data.py
+++ data.py
@@ -1,11 +1,12 @@
 import logging
-
+import re
+from time import time
 """
 Data shared between files
 """
 
 # Url to crawl
-DOMAIN = "https://crisp.chat/"
+DOMAIN = "https://docs.python.org"
 
 # Size of pool thread
 THREADS_COUNT = 20
@@ -21,6 +22,6 @@ websiteIndex = dict()
 # Create a file logger that will stores actions in web_crawler
 logger = logging.getLogger('web_crawler ')
 logger.setLevel(LOG_LEVEL)
-fh = logging.FileHandler('web_crawler.log')
+fh = logging.FileHandler('log/web_crawler_'+re.sub('[^A-Za-z0-9]+', '', DOMAIN)+'_'+str(time())+'.log')
 fh.setFormatter(logging.Formatter('%(asctime)s - %(message)s'))
 logger.addHandler(fh)
